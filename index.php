2ce2af5d710ce3ac08a369ab93cbf85ab6cc0032
saksit <huezhell@gmail.com>
2017-12-14 12:02:57 +0700

    ทดสอบฮิว

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
-//ทดสอบ
+//ทดสอบฮิว
 $app = new \Slim\App;
 $app->get('/hello',function()
 {
