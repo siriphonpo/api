32df2588a764b63f1acd2ecfe16242246064ef63
boat <siriphon.po@dld.go.th>
2017-12-14 10:27:53 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/hi',function()
+{
+    echo 'ว่าไงพวก';
+}
+);
+
+$app->get('/user/register', function()
+{
+   echo '/user/register';
+}
+);
+
+$app->get('/user/post', function() 
+{ 
+    echo '/user/post'; 
+});
+
+$app->get('/user/service', function() 
+{ 
+    echo '/user/service'; 
+});
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->run();
+
